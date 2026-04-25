c0b9242cec0e7e2807ef8f4d9074a6ff73c80f96
Sushant <sushantkhadka23@proton.me>
2026-04-25 16:09:36 +0545

    refactor(ports): update imports to lusay

--- internal/core/ports/ports.go
+++ internal/core/ports/ports.go
@@ -1,6 +1,6 @@
 package ports
 
-import "github.com/snufkin23/lucisay/internal/core/domain"
+import "github.com/snufkin23/lusay/internal/core/domain"
 
 // AIProvider is the port for interacting with an AI service
 type AIProvider interface {
